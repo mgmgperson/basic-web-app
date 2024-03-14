9a70fffa31864a174b40694f5786b392c7ab0032
Albert Lin <albert1104@gmail.com>
2024-03-14 16:13:15 -0400

    Added sq

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,7 +12,7 @@ export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("name")) {
     return "albert";
   }
-  if (query.toLowerCase().includes("which of the following numbers is the largest")) {
+  if (query.toLowerCase().includes("largest")) {
     const numbers = query.match(/\d+/g);
     if (numbers) {
       const parsedNumbers = numbers.map(Number);
@@ -24,6 +24,17 @@ export default function QueryProcessor(query: string): string {
     if (numbers) {
       const parsedNumbers = numbers.map(Number);
       return (parsedNumbers[0] + parsedNumbers[1]).toString();
+    }
+  }
+  if(query.toLowerCase().includes("square and a cube")){
+    const numbers = query.match(/\d+/g);
+    if (numbers) {
+      for (let i = 0; i<numbers.length ;i++){
+        const parsedNumbers = numbers.map(Number);
+        if (Math.sqrt(parsedNumbers[i]) % 1 === 0 && Math.cbrt(parsedNumbers[i]) % 1 === 0){
+          return parsedNumbers[i].toString();
+        }
+      }
     }
   }
 
