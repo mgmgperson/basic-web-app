e771fc3b9b998f4641a43a9e644da3768c93acbb
Albert Lin <albert1104@gmail.com>
2024-03-14 16:13:46 -0400

    Added mult

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -37,6 +37,14 @@ export default function QueryProcessor(query: string): string {
       }
     }
   }
+  if(query.toLowerCase().includes("multiplied")){
+    const numbers = query.match(/\d+/g);
+    if (numbers) {
+      const parsedNumbers = numbers.map(Number);
+      return (parsedNumbers[0] * parsedNumbers[1]).toString();
+    }
+  }
+
 
 
   return "";
